40227416d1aaa6fbe55e1531f25f02a87d13f0dc
851264559 <74701737+851264559@users.noreply.github.com>
2020-12-01 15:50:46 +0800

    更改主页

---
+++ js/menu_const.js
@@ -0,0 +1,12 @@
+//页面菜单映射【自行维护】
+var menuMapping = {
+	"sy001": "first.html",
+	"j003": "../CWL/StarEmployees.html",
+	"b001": "../CWL/ShiftTheMechanic.html",
+	"w002": "../CWL/FieldOfVehicle.html",
+	"w001": "../CWL/PickUpCarRegistration.html",
+	"j002": "../CWL/TestOnCompletion.html",
+	"f002": "../WZY/engineBrand.html",
+	"c001": "../WZY/vehicleFile.html",
+	"w003": "../WZY/maintenanceItems.html",
+}
